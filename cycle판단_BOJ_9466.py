d56729a90acc6e8c469df018efcf8ca743ae25e6
Umhyunsik <uhs2000@naver.com>
2021-06-13 18:15:08 +0900

    f c

---
+++ cycle판단_BOJ_9466.py
@@ -0,0 +1,28 @@
+def dfs(curr):
+    global count
+    check[curr]=True
+    next_=arr[curr]
+   # print(next_,"next",curr,"curr")
+    if check[next_]==True:
+        if not finish[next_]:
+            temp=next_
+            counts=1
+            while temp!=curr:
+              #  print(temp,curr)
+                temp=arr[temp]
+                counts+=1
+            count+=counts
+    else:
+        dfs(next_)
+    finish[curr]=True
+   
+N=int(input())
+arr=[0]+list(map(int,input().split()))
+check=[True]+[False]*N
+finish=[True]+[False]*N
+count=0
+
+for i in range(1,N+1):
+    if check[i]==False:
+        dfs(i)
+print(N-count)
